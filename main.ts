c49d05bc416c2cc7b3c4a2985d91f8bf20326851
DarkIzAGamer <93158945+DarkIzAGamer@users.noreply.github.com>
2021-10-25 14:52:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+/**
+ * Trenton Miller
+ */
+input.onButtonPressed(Button.A, function () {
+    PlayerAWins += 1
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    PlayersTie += 1
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    PlayerBWins += 1
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # . .
+        `)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Wins:")
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    basic.pause(1000)
+    basic.showNumber(PlayerAWins)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # . .
+        `)
+    basic.pause(1000)
+    basic.showNumber(PlayerBWins)
+    basic.pause(1000)
+    basic.showString("Ties:")
+    basic.showNumber(PlayersTie)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+let PlayersTie = 0
+let PlayerBWins = 0
+let PlayerAWins = 0
+PlayerAWins = 0
+PlayerBWins = 0
+PlayersTie = 0
